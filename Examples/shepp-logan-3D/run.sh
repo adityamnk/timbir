9d11ec9bfb2e5c4f17537d432f872aadc313c7bb
Kadri Aditya Mohan <mohank@conte-fe01.rcac.purdue.edu>
2015-08-27 15:35:13 -0400

    Fixed two bugs - 1. delta_t was being erroneously multiplied with ProjectionEntry in XT_VoxUpdate.c. 2. After object was initialized the slices werent communicated across nodes before updating.

--- Examples/shepp-logan-3D/run.sh
+++ Examples/shepp-logan-3D/run.sh
@@ -4,4 +4,4 @@ export OMP_NUM_THREADS=32
 uniq < $PBS_NODEFILE > nodefile
 
 # THE REGULARIZATION PARAMETERS USED IN THIS EXAMPLE ARE NOT OPTIMAL. IT IS JUST A WORKING CASE.
-mpiexec -n 1 -machinefile nodefile ../../Source_Code/reconstruct/basic/XT_Main --proj_rows 16 --proj_cols 256 --proj_num 256 --recon_num 1 --vox_wid 1 --rot_center 128 --sig_s 0.01 --sig_t 1 --c_s 0.00001 --c_t 0.00001 --convg_thresh 1 --remove_rings 0 
+mpiexec -n 2 -machinefile nodefile ../../Source_Code/reconstruct/basic/XT_Main --proj_rows 16 --proj_cols 256 --proj_num 256 --recon_num 1 --vox_wid 1 --rot_center 128 --sig_s 0.01 --sig_t 1 --c_s 0.00001 --c_t 0.00001 --convg_thresh 1 --remove_rings 0 --quad_convex 
