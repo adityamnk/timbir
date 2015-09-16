fbfb6545032e4c7f5fc35d874c1cf915df3953af
Singanallur Venkatakrishnan <svenkata@purdue.edu>
2015-09-16 10:31:27 -0700

    New branch to experiment at lbl

--- Examples/shepp-logan-3D/run.sh
+++ Examples/shepp-logan-3D/run.sh
@@ -1,7 +1,22 @@
-module load devel
-export PARALLEL=1
-export OMP_NUM_THREADS=32
-uniq < $PBS_NODEFILE > nodefile
+#!/bin/tcsh 
+# Example file for 3DMBIR code. Reconstruction a few slices from a data set from BL 8.3.2. by sub-setting the views. 
+
+#PBS -q regular
+#PBS -l mppwidth=96
+#PBS -l walltime=4:00:00
+#PBS -N PCT1
+#PBS -e PCT1.$PBS_JOBID.err
+#PBS -o PCT1.$PBS_JOBID.out
+#PBS -V
+
+cd $PBS_O_WORKDIR
+setenv OMP_NUM_THREADS 24
+setenv CRAY_ROOTFS DSL
+module load PrgEnv-intel
+module load python/2.7.3
+module load h5py
+module load pil
+module load mpi4py
 
 # THE REGULARIZATION PARAMETERS USED IN THIS EXAMPLE ARE NOT OPTIMAL. IT IS JUST A WORKING CASE.
-mpiexec -n 1 -machinefile nodefile ../../Source_Code/reconstruct/basic/XT_Main --proj_rows 16 --proj_cols 256 --proj_num 256 --recon_num 1 --vox_wid 1 --rot_center 128 --sig_s 0.01 --sig_t 1 --c_s 0.00001 --c_t 0.00001 --convg_thresh 1 --remove_rings 0 
+aprun -j 2 -n 1 -N 1 -d 24 -S 1 -ss -cc numa_node ../../Source_Code/reconstruct/basic/XT_Main --proj_rows 16 --proj_cols 256 --proj_num 256 --recon_num 1 --vox_wid 1 --rot_center 128 --sig_s 0.01 --sig_t 1 --c_s 0.00001 --c_t 0.00001 --convg_thresh 1 --remove_rings 0 
--- Source_Code/MBIR_4D/XT_AMatrix.c
+++ Source_Code/MBIR_4D/XT_AMatrix.c
@@ -43,7 +43,7 @@
 #include <math.h>
 #include "XT_IOMisc.h"
 
-/*'calcAMatrixColumnforAngle' computes the A matrix column for any voxel and angle of choice. This function is called repeatedy during ICD optimization. The indexing in angle is required since this code implements sparse angle sampling.
+/*'calcAMatrixColumnforAngle' computes the A matrix column for any voxel and angle of choice using an area weighted model. This function is called repeatedy during ICD optimization. The indexing in angle is required since this code implements sparse angle sampling.
 Ai - Pointer of A Matrix
 row, col - row and column of detector for which the A Matrix values are computed
 proj_idx - angle index at which A matrix values are computed for the given row, col*/
@@ -184,4 +184,71 @@ void compute_LapMatrix_4m_AMatrix(Sinogram* SinogramPtr, Real_t*** LapMatrix2D, 
 	
 		multifree(*AMatrix2DLine,1);
 	}
+}
+
+
+/*'calcAMatrixColumnforAngle_DD' computes the A matrix column for any voxel and angle of choice using a distance driven model. This function is called repeatedy during ICD optimization. The indexing in angle is required since this code implements sparse angle sampling.
+Ai - Pointer of A Matrix
+row, col - row and column of detector for which the A Matrix values are computed
+proj_idx - angle index at which A matrix values are computed for the given row, col*/
+void calcAMatrixColumnforAngle_DD (Sinogram* SinogramPtr, ScannedObject* ScannedObjectPtr, Real_arr_t** DetectorResponse, AMatrixCol *Ai, int32_t row, int32_t col, int32_t proj_idx)
+{
+  int32_t j;
+  Real_t x,y;
+  Real_t r;
+  Real_t rmax,rmin;
+  Real_t R_Center,delta_r;
+  Real_t w1,w2,f1;
+  int32_t index_min,index_max,index_delta_r;/*stores the detector index in which the profile lies*/
+  int32_t count = 0;
+
+  x = ScannedObjectPtr->x0 + ((Real_t)col+0.5)*ScannedObjectPtr->delta_xy;/*0.5 is for center of voxel. x_0 is the left corner*/
+  y = ScannedObjectPtr->y0 + ((Real_t)row+0.5)*ScannedObjectPtr->delta_xy;/*0.5 is for center of voxel. y_0 is the left corner*/
+/*'r' is the center of the voxel as projected on the detector. 'rmin' and 'rmax' gives the leftmost and rightmost distance at which the voxel of choice may have non-zero projection. We include some overhead*/
+    r = x*SinogramPtr->cosine[proj_idx] - y*SinogramPtr->sine[proj_idx];
+
+    rmin = r - ScannedObjectPtr->delta_xy;
+    rmax = r + ScannedObjectPtr->delta_xy;
+
+    if(rmax < SinogramPtr->R0 || rmin > SinogramPtr->RMax){
+	Ai->count = 0;
+        return;
+    }
+
+    index_min = (int32_t)(floor(((rmin - SinogramPtr->R0)/SinogramPtr->delta_r)));
+    index_max = (int32_t)(floor((rmax - SinogramPtr->R0)/SinogramPtr->delta_r));
+/*index_min and index_max is the quantized version of rmin and rmax*/
+
+    if(index_max >= SinogramPtr->N_r)
+      index_max = SinogramPtr->N_r - 1;
+
+    if(index_min < 0)
+      index_min = 0;
+	
+    for(j = index_min;j <= index_max; j++)/*Check*/
+    {
+
+      /*Accounting for different sensitivity across the detector*/
+      R_Center = (SinogramPtr->R0 + (((Real_t)j) + 0.5) *(SinogramPtr->delta_r));/*the 0.5 is to get to the center of the detector*/
+
+      /*Find the difference between the center of detector and center of projection and compute the Index to look up into*/
+      delta_r = fabs(r - R_Center);
+      index_delta_r = (int32_t)(floor((delta_r/SinogramPtr->OffsetR)));
+
+      if (index_delta_r >= 0 && index_delta_r < DETECTOR_RESPONSE_BINS)
+      {
+            w1 = delta_r - index_delta_r*SinogramPtr->OffsetR;
+	    w2 = SinogramPtr->OffsetR - w1;
+
+            f1 = (w2*DetectorResponse[proj_idx][index_delta_r] + w1*DetectorResponse[proj_idx][index_delta_r+1])/SinogramPtr->OffsetR;
+
+            if(f1 > 0)
+            {
+              Ai->values[count] = f1;
+              Ai->index[count] = j;
+              count++;
+            }
+       }
+    }
+    Ai->count = count;
 }
