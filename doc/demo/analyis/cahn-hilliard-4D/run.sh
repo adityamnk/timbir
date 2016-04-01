ecc4c5e4f12f5148221a87c17445ac2d9bc62c61
Kadri Aditya Mohan <mohank@rice-fe03.rcac.purdue.edu>
2016-04-01 12:02:11 -0400

    Fixed the reconstructions of demos of shepp-logan and cahn-hilliard

--- doc/demo/analyis/cahn-hilliard-4D/run.sh
+++ doc/demo/analyis/cahn-hilliard-4D/run.sh
@@ -1,8 +1,8 @@
-cd $PBS_O_WORKDIR
+#cd $PBS_O_WORKDIR
 module load devel
 export PARALLEL=1
 export OMP_NUM_THREADS=32
 uniq < $PBS_NODEFILE > nodefile
 
 # THE REGULARIZATION PARAMETERS USED IN THIS EXAMPLE ARE NOT OPTIMAL. IT IS JUST A WORKING CASE.
-mpiexec -n 1 -machinefile nodefile ../../../../src/reconstruct/basic/XT_Main --proj_rows 4 --proj_cols 256 --proj_num 256 --recon_num 8 --vox_wid 1 --rot_center 132 --sig_s 0.02 --sig_t 0.0005 --c_s 0.00001 --c_t 0.00001 --convg_thresh 1 --remove_rings 3 --huber_delta 0.1 --huber_T 3.0
+mpiexec -n 1 -machinefile nodefile ../../../../src/reconstruct/basic/XT_Main --proj_rows 4 --proj_cols 256 --proj_num 256 --recon_num 8 --vox_wid 1 --rot_center 132 --sig_s 0.02 --sig_t 0.0005 --c_s 0.00001 --c_t 0.00001 --convg_thresh 1 --remove_rings 3 
