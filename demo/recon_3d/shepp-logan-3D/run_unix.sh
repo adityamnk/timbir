b33e4495a88638234e1b6abce2a7cc0c8e90a048
Kadri Aditya Mohan <mohank@rice-fe02.rcac.purdue.edu>
2016-04-14 12:43:45 -0400

    Fixed the path to executables in the scripts

--- demo/recon_3d/shepp-logan-3D/run_unix.sh
+++ demo/recon_3d/shepp-logan-3D/run_unix.sh
@@ -8,4 +8,4 @@ make
 cd ../../../demo/recon_3d/shepp-logan-3D/
 
 # THE REGULARIZATION PARAMETERS USED IN THIS EXAMPLE ARE NOT OPTIMAL. IT IS JUST A WORKING CASE.
-mpiexec -n 1 ../../../src/reconstruct/basic/XT_Main --proj_rows 16 --proj_cols 256 --proj_num 256 --recon_num 1 --vox_wid 1 --rot_center 128 --sig_s 0.01 --sig_t 1 --c_s 0.00001 --c_t 0.00001 --convg_thresh 1 --remove_rings 0 --quad_convex 
+mpiexec -n 1 ../../../src/reconstruct/bin_data/XT_Main --proj_rows 16 --proj_cols 256 --proj_num 256 --recon_num 1 --vox_wid 1 --rot_center 128 --sig_s 0.01 --sig_t 1 --c_s 0.00001 --c_t 0.00001 --convg_thresh 1 --remove_rings 0 --quad_convex 
--- demo/recon_4d/cahn-hilliard-4D/run_cluster.sh
+++ demo/recon_4d/cahn-hilliard-4D/run_cluster.sh
@@ -8,10 +8,10 @@ uniq < $PBS_NODEFILE > nodefile
 cd ../../../src/MBIR_4D/
 make
 
-cd ../reconstruct/basic/
+cd ../reconstruct/bin_data/
 make
 
 cd ../../../demo/recon_3d/shepp-logan-3D/
 
 # THE REGULARIZATION PARAMETERS USED IN THIS EXAMPLE ARE NOT OPTIMAL. IT IS JUST A WORKING CASE.
-mpiexec -n 1 -machinefile nodefile ../../../src/reconstruct/basic/XT_Main --proj_rows 4 --proj_cols 256 --proj_num 256 --recon_num 8 --vox_wid 1 --rot_center 132 --sig_s 0.02 --sig_t 0.0005 --c_s 0.00001 --c_t 0.00001 --convg_thresh 1 --remove_rings 3 
+mpiexec -n 1 -machinefile nodefile ../../../src/reconstruct/bin_data/XT_Main --proj_rows 4 --proj_cols 256 --proj_num 256 --recon_num 8 --vox_wid 1 --rot_center 132 --sig_s 0.02 --sig_t 0.0005 --c_s 0.00001 --c_t 0.00001 --convg_thresh 1 --remove_rings 3 
